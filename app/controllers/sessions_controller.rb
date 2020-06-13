f71af59ac67613e550cab593bac3053f2791ae7f
sourabkommawar <sourabkommawar@gmail.com>
2020-06-13 13:02:46 +0530

    Added remember me functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
   	user =User.find_by(email: user_email)
   	if user && user.authenticate(params[:session][:password])
   		log_in user
+      flash[:succes]="Welcome!"
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user_url(user)
   	else
   		flash[:danger]="Email or Password Incorrect."
@@ -15,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+	attr_accessor :remember_token
+
 	VALID_REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true ,length: {maximum: 50}
 	validates :email, presence: true , length: {maximum: 255},
@@ -6,4 +9,27 @@ class User < ApplicationRecord
 						uniqueness: {case_sensitive: false}
 	validates :password, presence: true, length: {minimum: 6}
 	has_secure_password
+
+	def User.new_token
+		return SecureRandom.urlsafe_base64
+	end
+
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+		BCrypt::Password.create(string,cost: cost)
+	end
+
+	def remember
+		self.remember_token=User.new_token
+		update_attribute(:remember_digest , User.digest(remember_token))
+	end
+
+	def authenticated?(remember_token)
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+
 end
